40775a67619849bee2eab94e0719be263983a2b2
garantor <afolabisunday31@yahoo.com>
2019-06-08 09:00:35 +0100

    django web project for practice555 Model Buggy Version

--- page1/models.py
+++ page1/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class new_user(models.Model):
 
     username = models.CharField(max_length= 100)
@@ -9,3 +10,6 @@ class new_user(models.Model):
     email = models.CharField(max_length= 200)
     number = models.IntegerField()
     img = models.ImageField(max_length=1000)
+
+
+    there is a bug of environment veriable here that needs to be fixed
